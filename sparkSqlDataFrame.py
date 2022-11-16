f2bfbaac95bade287a1842db699c231c30e99b67
Konrad Dziedzina <dziedzina.konrad@gmail.com>
2022-11-16 21:46:47 +0100

    Added new dataset and add new dataset

---
+++ sparkSqlDataFrame.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, IntegerType
+from os import getcwd
+
+schema = StructType([
+                  StructField("Station", StringType(), True),
+                  StructField("Date", StringType(), True),
+                  StructField("Measures", StringType(), True),
+                  StructField("Temp", IntegerType(), True)
+                  ])
+
+spark = SparkSession.builder.appName('tempMeasures').getOrCreate()
+df = spark.read.schema(schema).csv(getcwd() + '/Datasets/1800.csv')
