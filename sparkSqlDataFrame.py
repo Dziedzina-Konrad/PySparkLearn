c247fdd9866bd97d0c3261c6d3734d1bdbf69170
Konrad Dziedzina <dziedzina.konrad@gmail.com>
2022-11-17 19:09:59 +0100

    Corrected few details in previous query

--- sparkSqlDataFrame.py
+++ sparkSqlDataFrame.py
@@ -13,8 +13,9 @@ spark = SparkSession.builder.appName('tempMeasures').getOrCreate()
 df = spark.read.schema(schema).csv(getcwd() + '/Datasets/1800.csv')
 df.createOrReplaceTempView('stationTemp')
 spark.sql('''SELECT DISTINCT Station, 
+          Measures, 
           MIN(Temp) OVER(PARTITION BY Station, Measures) AS MinTemp,
-          MAX(Temp) OVER(PARTITION BY Station, Measures) AS MinTemp,
+          MAX(Temp) OVER(PARTITION BY Station, Measures) AS MaxTemp,
           FIRST_VALUE(DATE) OVER(PARTITION BY Station, Measures ORDER BY Temp) AS DateOfMinTemp,
           FIRST_VALUE(DATE) OVER(PARTITION BY Station, Measures ORDER BY Temp DESC) AS DateOfMaxTemp
           FROM stationTemp
