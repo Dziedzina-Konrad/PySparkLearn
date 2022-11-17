4396625392500be389e810a1073307a724bb00da
Konrad Dziedzina <dziedzina.konrad@gmail.com>
2022-11-17 19:04:23 +0100

    Modified query

--- sparkSqlDataFrame.py
+++ sparkSqlDataFrame.py
@@ -12,4 +12,10 @@ schema = StructType([
 spark = SparkSession.builder.appName('tempMeasures').getOrCreate()
 df = spark.read.schema(schema).csv(getcwd() + '/Datasets/1800.csv')
 df.createOrReplaceTempView('stationTemp')
-spark.sql('SELECT * FROM stationTemp').show(50)
+spark.sql('''SELECT DISTINCT Station, 
+          MIN(Temp) OVER(PARTITION BY Station, Measures) AS MinTemp,
+          MAX(Temp) OVER(PARTITION BY Station, Measures) AS MinTemp,
+          FIRST_VALUE(DATE) OVER(PARTITION BY Station, Measures ORDER BY Temp) AS DateOfMinTemp,
+          FIRST_VALUE(DATE) OVER(PARTITION BY Station, Measures ORDER BY Temp DESC) AS DateOfMaxTemp
+          FROM stationTemp
+          ORDER BY Station''').show(100)
